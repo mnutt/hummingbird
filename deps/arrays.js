eaad5ca2ef083dcb0c5d4d61ea506bf0583b410d
Michael Nutt <mnutt@gilt.com>
2010-04-25 18:44:40 -0400

    Trying out gource log script

--- deps/arrays.js
+++ deps/arrays.js
@@ -6,3 +6,11 @@ Array.prototype.remove = function(e) {
 Array.prototype.each = function(fn) {
   for (var i = 0; i < this.length; i++) fn(this[i]);
 }
+
+Array.prototype.compact = function(fn) {
+  for (var i = 0; i < this.length; i++)
+    if (this[i] == null) {
+      this.splice(i, 1);
+    }
+  return this;
+}

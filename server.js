2d28a481152a02b3cffe31718d3712654b030d1c
Michael Nutt <mnutt@gilt.com>
2010-04-28 15:49:57 -0400

    Add a weekly json feed

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ require.paths.unshift(__dirname);
 var sys = require('sys'),
   http = require('http'),
   ws = require('deps/node-ws/ws'),
+  proxy = require('proxy'),
+  weekly = require('weekly'),
   mongo = require('deps/node-mongodb-native/lib/mongodb'),
   Hummingbird = require('hummingbird').Hummingbird;
 
@@ -43,8 +45,25 @@ db.open(function(p_db) {
       sys.log("ws close");
     });
   }).listen(WEB_SOCKET_PORT);
-});
 
-sys.puts('Web Socket server running at ws://localhost:' + WEB_SOCKET_PORT);
-var staticAssets = require('static_assets');
-staticAssets.serveStatic(MONITOR_PORT);
+  sys.puts('Web Socket server running at ws://localhost:' + WEB_SOCKET_PORT);
+  var staticAssets = require('static_assets');
+
+  try {
+    http.createServer(function(req, res) {
+      if(req.url.match(/\/sale_list/)) {
+        proxy.route("/sale_list",
+                    "http://www.gilt.com/pagegen_service/sale/sale_list", req, res);
+      } else if(req.url.match(/\/week.json/)) {
+        weekly.serve(db, req, res);
+      } else {
+        staticAssets.serveStatic(req, res);
+      }
+    }).listen(MONITOR_PORT);
+
+    sys.puts('Analytics server running at http://localhost:' + MONITOR_PORT + '/');
+  } catch(e) {
+    sys.puts('Detected webserver already running on http://localhost:' + MONITOR_PORT + '.');
+  }
+
+});

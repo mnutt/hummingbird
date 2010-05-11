b658ea1228df9571bcb0f69506feca72157ef12f
Michael Nutt <mnutt@gilt.com>
2010-05-11 01:06:16 -0400

    Add referrer to hb pixel

--- hummingbird.js
+++ hummingbird.js
@@ -11,6 +11,9 @@ HummingbirdTracker.track = function(env) {
     env.gen = document.cookie.match(/gender=([^;]*);/)[1];
     env.uid = document.cookie.match(/user_id=([^\_]*)_([^;]*)/)[2];
   }
+  if(document.referrer && document.referrer != "") {
+    env.ref = document.referrer;
+  }
 
   $('body').append('<img src="http://demo.hummingbirdstats.com/tracking.gif?' + jQuery.param(env) + '"/>');
 };

139222fbc6741750dd9ecc9e052adf49a72a3d14
Michael Nutt <mnutt@gilt.com>
2010-05-18 17:52:10 -0400

    Switch to miksago's node-websocket library; update tracking server to use config

--- server.js
+++ server.js
@@ -3,16 +3,23 @@ require.paths.unshift(__dirname);
 
 var sys = require('sys'),
   http = require('http'),
-  ws = require('deps/node-ws/ws'),
   weekly = require('weekly'),
+  fs = require('fs'),
   mongo = require('deps/node-mongodb-native/lib/mongodb'),
   Hummingbird = require('hummingbird').Hummingbird;
 
-var TRACKING_PORT = 8000,
-    WEB_SOCKET_PORT = 8080,
-    MONITOR_PORT = 8888;
+try {
+  var configJSON = fs.readFileSync(__dirname + "/config/app.json");
+} catch(e) {
+  sys.log("File config/app.json not found.  Try: `cp config/app.json.sample config/app.json`");
+}
+var config = JSON.parse(configJSON);
 
 db = new mongo.Db('hummingbird', new mongo.Server('localhost', 27017, {}), {});
+
+db.addListener("error", function(error) {
+  sys.puts("Error connecting to mongo -- perhaps it isn't running?");
+});
 
 db.open(function(p_db) {
   var hummingbird = new Hummingbird(db, function() {
@@ -22,28 +29,8 @@ db.open(function(p_db) {
       } catch(e) {
         hummingbird.handleError(req, res, e);
       }
-    }).listen(TRACKING_PORT);
+    }).listen(config.tracking_port);
   });
 
-  sys.puts('Tracking server running at http://*:' + TRACKING_PORT + '/tracking_pixel.gif');
-
-  // Websocket TCP server
-  ws.createServer(function (websocket) {
-    websocket.addListener("connect", function (resource) {
-      // emitted after handshake
-      sys.log("ws connect: " + resource);
-      if (resource === '/') {
-        hummingbird.addClient(websocket);
-      }
-      else if (resource === '/aggregates') {
-        hummingbird.serveAggregates(websocket);
-      }
-    }).addListener("close", function () {
-      // emitted when server or client closes connection
-      hummingbird.removeClient(websocket);
-      sys.log("ws close");
-    });
-  }).listen(WEB_SOCKET_PORT);
-
-  sys.puts('Web Socket server running at ws://*:' + WEB_SOCKET_PORT);
+  sys.puts('Tracking server running at http://*:' + config.tracking_port + '/tracking_pixel.gif');
 });

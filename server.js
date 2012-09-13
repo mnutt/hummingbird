5267069398586811bf1d831d118306ad36e2acf1
Michael Nutt <michael@nuttnet.net>
2012-09-13 02:02:01 -0400

    split up hummingbird into a tracker and a dashboard; clean up metric api

--- server.js
+++ server.js
@@ -1,71 +1,24 @@
-var http = require('http'),
-  weekly = require('./lib/weekly'),
-  config = require('./config/config'),
-  dgram = require('dgram'),
-  static = require('node-static'),
-  sio = require('socket.io'),
-  mongo = require('mongodb'),
-  Hummingbird = require('./lib/hummingbird').Hummingbird;
+config = require('./config/config');
 
-db = new mongo.Db('hummingbird', new mongo.Server(config.mongo_host, config.mongo_port, {}), {});
-
-db.addListener("error", function(error) {
-  console.log("Error connecting to mongo -- perhaps it isn't running?");
-});
-
-db.open(function(p_db) {
-  var hummingbird = new Hummingbird();
-  hummingbird.init(db, function() {
-    var server = http.createServer(function(req, res) {
-      try {
-        hummingbird.serveRequest(req, res);
-      } catch(e) {
-        hummingbird.handleError(req, res, e);
-      }
-    });
-    server.listen(config.tracking_port, "0.0.0.0");
+var dashboard = require('./lib/dashboard');
+var tracker = require('./lib/tracker');
 
 
-    if(config.enable_dashboard) {
-      var file = new(static.Server)('./public');
+dashboard.listen(config.dashboard_port, config.dashboard_address);
+console.log("Dashboard listening on http://" + (config.dashboard_address || '*') + ":" + config.dashboard_port + ".");
 
-      var dashboardServer = http.createServer(function (request, response) {
-        request.addListener('end', function () {
-          file.serve(request, response);
-        });
-      });
 
-      dashboardServer.listen(config.dashboard_port);
+// Tracker should listen on the same port as the dashboard
+tracker.listen(dashboard);
+console.log("Tracker listening on http://" + (config.dashboard_address || '*') + ":" + config.dashboard_port + "/tracking_pixel.gif.");
 
-      io = sio.listen(dashboardServer);
 
-      console.log('Dashboard server running at http://*:' + config.dashboard_port);
-    } else {
-      io = sio.listen(server);
-    }
+// If you want to have the tracking pixel listen on a different port
+// (for instance in order to password-protect your dashboard) you can
+// uncomment this
+//
+// tracker.listen(8000, "0.0.0.0");
 
-    io.set('log level', 2);
-
-    hummingbird.io = io;
-    hummingbird.addAllMetrics(io, db);
-
-    console.log('Web Socket server running at ws://*:' + config.tracking_port);
-
-    if(config.udp_address) {
-      var udpServer = dgram.createSocket("udp4");
-
-      udpServer.on("message", function(message, rinfo) {
-        console.log("message from " + rinfo.address + " : " + rinfo.port);
-
-        var data = JSON.parse(message);
-        hummingbird.insertData(data);
-      });
-
-      udpServer.bind(config.udp_port, config.udp_address);
-
-      console.log('UDP server running on UDP port ' + config.udp_port);
-    }
-  });
-
-  console.log('Tracking server running at http://*:' + config.tracking_port + '/tracking_pixel.gif');
-});
+// UDP tracking
+//
+// tracker.listenUdp(8000, "0.0.0.0");
